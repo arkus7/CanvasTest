4c7925751eff374f9d0a3ac99aeccd7cfaa3e0e8
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-16 21:22:31 +0100

    add random color to circles

--- app/src/main/java/com/mooduplabs/canvas/Entity.java
+++ app/src/main/java/com/mooduplabs/canvas/Entity.java
@@ -1,16 +1,26 @@
 package com.mooduplabs.canvas;
 
+import android.graphics.Color;
+
+import java.util.Random;
+
 public class Entity {
     private int x;
     private int y;
     private String iconPath;
     private int speed;
     private double radius;
+    private int color;
 
     public Entity(int x, int y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+        Random rnd = new Random();
+        this.color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+    public void setColor(int color) {
+        this.color = color;
     }
 
     public int getX() {
