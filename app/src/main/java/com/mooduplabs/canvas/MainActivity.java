84035c51c3daf5b0c6bb2cb610b501f065553e1c
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-16 21:21:09 +0100

    add butterknife

--- app/src/main/java/com/mooduplabs/canvas/MainActivity.java
+++ app/src/main/java/com/mooduplabs/canvas/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+    @Bind(R.id.canvas)
+    SurfaceView surfaceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
